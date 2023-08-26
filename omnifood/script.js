6d5a1ebe959d9bc159c3ec585ac3dbee323cfc43
vikas.t <vikas.t@pepperfry.com>
2023-08-26 11:37:56 +0530

    linked links and smooth scroll

--- omnifood/script.js
+++ omnifood/script.js
@@ -9,3 +9,30 @@ const headerEl = document.querySelector('.header');
 navBtnEl.addEventListener('click',() => {
     headerEl.classList.toggle('nav-open');
 });
+
+// Smooth Scrolling
+// This also doesn't work bydefault in safari so we need to import package for this
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const href = link.getAttribute("href");
+        // scroll to top i.e "#"
+        if(href === "#") {
+            window.scrollTo({
+                top:0,
+                behavior: "smooth"
+            });
+        }
+        // scroll to other sections with id
+        if(href !== "#" && href.startsWith("#")){
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({behavior:"smooth"});  
+        }
+
+        // If link is clicked from mobile nav close drawer
+        if(link.classList.contains("main-nav-link")){
+            headerEl.classList.toggle("nav-open");
+        }
+    });
+});
